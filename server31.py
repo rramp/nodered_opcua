804700dc69c8f3a4dc5fc0bc20bfdf76af66001c
rramp <48037783+rramp@users.noreply.github.com>
2022-01-07 12:12:28 -0300

    Add files via upload

---
+++ server31.py
@@ -0,0 +1,40 @@
+
+import sys
+sys.path.insert(0, "..")
+import time
+import numpy as np
+
+from opcua import ua, Server
+
+
+if __name__ == "__main__":
+
+    # setup our server
+    server = Server()
+    server.set_endpoint("opc.tcp://0.0.0.0:4840")
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = server.register_namespace(uri)
+
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+
+    # populating our address space
+    myobj = objects.add_object(idx, "MyObject")
+    myvar = myobj.add_variable(idx, "MyVariable", 6.7)
+    myvar1 = myobj.add_variable(idx, "MyVariable1",0.0)
+    myvar.set_writable()    # Set MyVariable to be writable by clients
+    myvar1.set_writable() 
+    # starting!
+    server.start()
+    
+    try:
+        count = 0
+        while True:
+            time.sleep(1)
+            count += 0.1
+            myvar.set_value(count)
+    finally:
+        #close connection, remove subcsriptions, etc
+        server.stop()
